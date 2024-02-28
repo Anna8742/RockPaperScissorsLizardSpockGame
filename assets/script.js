8fe0acebecb59a566bc5d347832c22d56282265a
Anna8742 P <anna.papka014@gmail.com>
2024-02-28 20:05:55 +0000

    added variables to js file

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,27 @@
-// function to prompt user to enter name
-
-function enterName() {
-    let text;
-    let user = prompt("Please enter your name:");
+//Prompt user to enter name when the page loads
+var text;
+    var user = prompt("Please enter your name to start the game:");
     if (user == null || user == "") {
-        text = "Cancelled the game.";
+        alert("Wrong input. Cancelled the game.");
+        location.reload();
     } else {
-        text = user + " ";
+        text = user;
     }
-    document.getElementById("userName").innerHTML = text.toUpperCase();
-    document.getElementById("player").innerHTML = text.toUpperCase();
-}
+document.getElementById("userName").innerHTML = text.toUpperCase();
+document.getElementById("player").innerHTML = text.toUpperCase();
+
+// Time limit in seconds
+var timer = 15; 
+// Variable to store 1 second time interval 
+var timerInterval;
+// Array of possible choices
+var choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+// Variable to store computer's score
+var computerScore = 0;
+// Variable to store player's score
+var playerScore = 0;
+// Variable to store left rounds to play
+var roundsLeft = 10;
+// Variable to store the 
+var result_of_choice;
+
